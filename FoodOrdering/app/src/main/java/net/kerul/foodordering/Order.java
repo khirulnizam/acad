3718ada86bd9552382661c5f9eb587c88194a036
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2021-01-12 12:55:37 +0800

    Day2 Spinner

--- FoodOrdering/app/src/main/java/net/kerul/foodordering/Order.java
+++ FoodOrdering/app/src/main/java/net/kerul/foodordering/Order.java
@@ -3,12 +3,52 @@ package net.kerul.foodordering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Order extends AppCompatActivity {
+public class Order extends AppCompatActivity implements OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        //spinner as object
+        Spinner spinner = findViewById(R.id.phoneOptions);
+        if(spinner!=null){
+            spinner.setOnItemSelectedListener(this);
+        }
+        //provide the options of the spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.phoneoptions, android.R.layout.simple_spinner_item);
+    }
+
+    public void onRadioClick(View view){
+        boolean check = ((RadioButton) view).isChecked();
+        if (view.getId()==R.id.sameday){
+            Toast.makeText(this, "Sameday chosen", Toast.LENGTH_SHORT).show();
+        }
+        else if (view.getId()==R.id.nextday){
+            Toast.makeText(this, "Next chosen", Toast.LENGTH_SHORT).show();
+        }
+        else if (view.getId()==R.id.pickup){
+            Toast.makeText(this, "Pickup chosen", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
