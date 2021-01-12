aba637ebe14c5893ef7fba73960b5b0b271aecca
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2021-01-12 16:12:03 +0800

    simple date picker widget

--- FoodOrdering/app/src/main/java/net/kerul/foodordering/DatePickerActivity.java
+++ FoodOrdering/app/src/main/java/net/kerul/foodordering/DatePickerActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class DatePickerActivity extends AppCompatActivity {
-
+    DatePicker datepicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_picker);
+
+        //date picker
+        datepicker=(DatePicker)findViewById(R.id.datePicker1);
+    }
+
+    //
+    public void simpledatepick(View view){
+        String date = datepicker.getDayOfMonth()+"/"+(datepicker.getMonth()+1)+"/"+datepicker.getYear();
+        Toast.makeText(this,date,Toast.LENGTH_SHORT).show();
     }
 
     //function called by burron click from Activity
-    public void showDatePicker(){
+    public void showDatePicker(View view){
         DialogFragment newFragment = new DatePickerFragment();
-        //newFragment.show(getActivity(), this, year,month,day);
+        newFragment.show(getSupportFragmentManager(),"datePicker");
     }
 }
