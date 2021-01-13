c6c1b980e58cfaad2464e135411d3b33ce3bb524
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2021-01-13 10:14:32 +0800

    Recycler view display

--- RecyclerEx/app/src/main/java/net/kerul/recyclerex/MainActivity.java
+++ RecyclerEx/app/src/main/java/net/kerul/recyclerex/MainActivity.java
@@ -1,14 +1,33 @@
 package net.kerul.recyclerex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.LinkedList;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+    private final LinkedList<String> mWordList = new LinkedList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //populate data to LinkedList
+        mWordList.add("Learning Android Studio 4");
+        mWordList.add("Layout XML");
+        mWordList.add("Java Programming");
+        //display the words
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
